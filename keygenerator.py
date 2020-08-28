61c357577547d2f1c0b30d11bf904a95fb6138b6
isakwaldener <isak_waldener@hotmail.com>
2020-08-28 23:22:56 +0200

    Update keygenerator.py

--- keygenerator.py
+++ keygenerator.py
@@ -6,54 +6,65 @@ pygame.init()
 # maybe use generator instead of list for the keys to save memory
 
 class keygenerator():
-    modes = {"upperCase": (65, 90),
-             "allKeys": (32, 122),
-             "lowerCase": (97, 122),
-             "abc": (97, 122)}
 
     def __init__(self):
         self.mode = None
         self.keys = []
 
 
-    def generateNewKeys(self):
+    def generate_new_keys(self):
         self.set_mode_keys()
-        if self.mode != "abc":
-            random.shuffle(self.keys) 
+        self.check_if_shuffle()
+    
+    def check_if_shuffle(self):
+        if self.mode.should_shuffle():
+            random.shuffle(self.keys)
 
     def set_mode_keys(self):
         lower, upper = self.get_mode_limits()
         self.keys = [i for i in range(lower, upper)]
 
-    def getKey(self, num):
+    def get_key(self, num):
         if not self.keys:
-            self.generateNewKeys()
+            self.generate_new_keys()
         key = self.keys[num]
         return key
 
-    def removeFirstKey(self):
+    def remove_first_key(self):
         del self.keys[0]
 
-    def getKeys(self):
+    def get_keys(self):
         if not self.keys:
-            self.generateNewKeys()
+            self.generate_new_keys()
         return self.keys
 
-    def setMode(self, mode):
+    def set_mode(self, mode):
         self.mode = mode
 
     def get_mode_name(self):
         return self.mode
 
     def get_mode_limits(self):
-        limits = self.modes[self.mode]
+        limits = self.mode.get_limits()
         return limits
 
-    def checkUpperKeys(self, key, mod):
-        if key == self.getKey(0) + 32:
+    def check_if_correct_key(self, key, mod):
+        if self.mode.get_name() == "upper":
+            return self.check_upper_keys(key, mod)
+        else:
+            return self.check_lower_keys(key)
+
+    def check_key_test(self, key, mod):
+        if self.get_key(0) >= 32 and self.get_key(0) <= 90:
+            return self.check_upper_keys(key, mod)
+        else:
+            return self.check_lower_keys(key)
+
+    def check_upper_keys(self, key, mod):
+        if key == self.get_key(0) + 32:
             return self.check_if_shift_down(mod)
         else: 
-            self.check_if_shift_released(key)
+            return self.check_if_shift_released(key)
     
     def check_if_shift_down(self, mod):
         if mod & pygame.KMOD_LSHIFT or mod & pygame.KMOD_RSHIFT:
@@ -61,30 +72,13 @@ class keygenerator():
         return False
 
     def check_if_shift_released(self, key):
-        lshift = pygame.KMOD_LSHIFT
-        rshift = pygame.KMOD_RSHIFT
+        lshift = pygame.K_LSHIFT
+        rshift = pygame.K_RSHIFT
         if key == lshift or key == rshift:
             return None
         return False
-
-    def checkLowerKeys(self, key):
-        if key == self.getKey(0):
+    
+    def check_lower_keys(self, key):
+        if key == self.get_key(0):
             return True
         return False
-
-    def checkMods(self, key, mod):
-        correctKey = None
-
-        if self.mode is None:  # if something bugs and mode is none
-            correctKey = self.checkLowerKeys(key)
-        elif self.mode[0] == 65 and self.mode[1] == 90:  # checks upper
-            correctKey = self.checkUpperKeys(key, mod)
-        else:
-            correctKey = self.checkLowerKeys(key)
-
-        if correctKey is None:
-            return False, False  # gameover, correctKey
-        elif correctKey:
-            return False, correctKey
-        else:
-            return True, correctKey
