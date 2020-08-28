7806b6549c6b98711b1d4791fbf1755ec5c04888
isakwaldener <isak_waldener@hotmail.com>
2020-08-28 23:22:53 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -23,7 +23,7 @@ class gui():
         # draw buttons for different modes and highscores
         font = pygame.font.Font(None, 30)
         self.board.fill((250, 250, 250))
-        chars = [chr(i) for i in self.game.getKeys()]
+        chars = [chr(i) for i in self.game.get_keys()]
 
         text = font.render(f"{' '.join(chars)}", 1, (0, 0, 0))
         self.board.blit(text, (10, 200))
@@ -37,15 +37,15 @@ class gui():
     def drawEndScreen(self):
         font = pygame.font.Font(None, 30)
         self.board.fill((250, 250, 250))
-        points = self.game.getPoints()
+        points = self.game.get_points()
         textPoint = font.render(f"RIP! You got {points} Points", 10, (0, 0, 0))
         self.board.blit(textPoint, (100, 100))
 
-        time = self.game.getTotalTimeInSec()
+        time = self.game.get_total_time_in_sec()
         textTime = font.render(f"Total game time: {time}", 10, (0, 0, 0))
         self.board.blit(textTime, (100, 150))
 
-        wpm = self.game.getWPM()
+        wpm = self.game.get_words_per_min()
         textWPM = font.render(f"Words per minute: {wpm}", 10, (0, 0, 0))
         self.board.blit(textWPM, (100, 200))
 
