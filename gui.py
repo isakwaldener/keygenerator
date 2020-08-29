56f0008cc5d0db833f752d8be5ca3db309b096e7
isakwaldener <isak_waldener@hotmail.com>
2020-08-29 22:12:10 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -5,50 +5,58 @@ pygame.init()
 class gui():
     def __init__(self, game):
         self.disp = pygame.display.set_mode((450, 450))
-        self.board = self.boardinit()
+        self.board = self.board_init()
         self.game = game
 
-    def boardinit(self):
+    def board_init(self):
         background = pygame.Surface(self.disp.get_size())
         background = background.convert()
         background.fill((250, 250, 250))
         return background
 
-    def updateBoard(self):
-        self.drawBoard()
+    def update_board(self):
+        self.add_keys_to_board()
         self.disp.blit(self.board, (0, 0))
         pygame.display.flip()
 
-    def drawBoard(self):
-        # draw buttons for different modes and highscores
-        font = pygame.font.Font(None, 30)
-        self.board.fill((250, 250, 250))
-        chars = [chr(i) for i in self.game.get_keys()]
+    def add_keys_to_board(self):
+        self.draw_black_background()
+        chars = self.get_chars_from_keys() 
+        self.draw_keys_to_board(chars)
 
+    def get_chars_from_keys(self):
+        return [chr(i) for i in self.game.get_keys()]
+
+    def draw_keys_to_board(self, chars):
+        font = pygame.font.Font(None, 30)
         text = font.render(f"{' '.join(chars)}", 1, (0, 0, 0))
         self.board.blit(text, (10, 200))
 
-    def drawStatus(self, char, height, width):
+    def draw_end_screen(self):
         font = pygame.font.Font(None, 30)
-        self.board.fill((250, 250, 250))
-        text = font.render(f"{char}", 10, (0, 0, 0))
-        return text
+        self.draw_black_background()
+        self.add_points_to_endscreen(font)
+        self.add_time_to_endscreen(font)
+        self.add_wpm_to_endscreen(font)
 
-    def drawEndScreen(self):
-        font = pygame.font.Font(None, 30)
-        self.board.fill((250, 250, 250))
+        self.disp.blit(self.board, (0, 0))
+        pygame.display.flip()
+
+    def add_points_to_endscreen(self, font):
         points = self.game.get_points()
         textPoint = font.render(f"RIP! You got {points} Points", 10, (0, 0, 0))
         self.board.blit(textPoint, (100, 100))
-
+        
+    def add_time_to_endscreen(self, font):
         time = self.game.get_total_time_in_sec()
         textTime = font.render(f"Total game time: {time}", 10, (0, 0, 0))
         self.board.blit(textTime, (100, 150))
 
+    def add_wpm_to_endscreen(self, font):
         wpm = self.game.get_words_per_min()
         textWPM = font.render(f"Words per minute: {wpm}", 10, (0, 0, 0))
         self.board.blit(textWPM, (100, 200))
 
-        self.disp.blit(self.board, (0, 0))
-        pygame.display.flip()
+    def draw_black_background(self):
+        self.board.fill((250, 250, 250))
 
