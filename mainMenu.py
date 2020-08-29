6fb328c73753be4191f77b7cd99b5edd6ab4ae21
isakwaldener <isak_waldener@hotmail.com>
2020-08-29 22:12:15 +0200

    Update mainMenu.py

--- mainMenu.py
+++ mainMenu.py
@@ -5,27 +5,42 @@ from Modes import Modes
 class mainMenu:
         def __init__(self, root):
             self.root = root
-            root.title("mainMenu")
             self.game = game()
+            self.init_gui()
 
-            self.label = Label(root, text="Choose mode")
-            self.label.pack()
+        def init_gui(self):
+            self.root.title("mainMenu")
+            self.create_label("choose mode")
+            self.add_buttons()
 
-            self.lower_button = Button(root, text="lowerCase", command=self.lower_case_game)
+        def add_buttons(self):
+            self.lower_case_button()
+            self.upper_case_button()
+            self.abc_case_button()
+
+        def create_label(self, text):
+            label = Label(self.root, text=text)
+            label.pack()
+
+        def lower_case_button(self):
+            self.lower_button = Button(self.root, text="lowerCase", command=self.lower_case_game)
             self.lower_button.pack()
             self.root.bind('1', lambda event: self.lower_case_game())
 
-            self.upper_button = Button(root, text="upperCase", command=self.upper_case_game)
+        def upper_case_button(self):
+            self.upper_button = Button(self.root, text="upperCase", command=self.upper_case_game)
             self.upper_button.pack()
             self.root.bind('2', lambda event: self.upper_case_game())
 
-            self.abc_button = Button(root, text="abc", command=self.abc_game)
+        def abc_case_button(self):
+            self.abc_button = Button(self.root, text="abc", command=self.abc_game)
             self.abc_button.pack()
             self.root.bind('3', lambda event: self.abc_game())
 
-            # self.all_button = Button(root, text="allKeys", command=self.all_game)
-            # self.all_button.pack()
-            # self.root.bind('4', lambda event: self.lower_case_game())
+        def all_case_button(self):
+            self.all_button = Button(self.root, text="allKeys", command=self.all_game)
+            self.all_button.pack()
+            self.root.bind('4', lambda event: self.lower_case_game())
 
         def lower_case_game(self):
             self.root.destroy()
