f2e62c4a3f28fe75521e3dab93ee1bff5333b640
isakwaldener <isak_waldener@hotmail.com>
2020-08-30 11:48:29 +0200

    Update Modes.py

--- Modes.py
+++ Modes.py
@@ -1,10 +1,9 @@
 from enum import Enum
 
 class Modes(Enum):
-    abc = ((97,122), "lower", False)
-    lower = ((97,122), "lower", True) 
-    upper = ((65, 90), "upper", True)
-    all_keys = ((32, 122), "both", True)
+    abc = ((97,122), False)
+    lower = ((97,122), True) 
+    upper = ((65, 90), True)
     # reverse mode maybe
 
     def get_name(self):
@@ -13,10 +12,6 @@ class Modes(Enum):
     def get_limits(self):
         limits = self.value[0]
         return limits
-    
-    def get_mode_type(self):
-        mode_type = self.value[1]
-        return mode_type
 
     def should_shuffle(self):
-        return self.value[2]
+        return self.value[1]
